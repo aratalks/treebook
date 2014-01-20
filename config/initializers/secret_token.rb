8443b1e024424c83673631963893168535fa7ccf
Argie Gallego <argie_gallego@yahoo.com>
2014-01-20 23:33:59 +0800

    Creates FB status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '58f750b47be892da884e191d672bc072b1125225095c15ba5699fca18dd501d1eb066e05f06e5e2c4bf0d159fb5a572e88734ac21a1a6ee5b6d79fed75799051'
